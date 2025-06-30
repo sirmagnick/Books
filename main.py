ffd062b6582831c23dd12321b9b33df4803931ab
sirmagnick <magnick@interia.pl>
2025-06-30 15:24:57 +0200

    Replace test1 with pipe-maze and add submodule

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import pipe_maze
 
 st.title("activity books generator")
 
-if st.button("test1"):
-    st.write("Test1 clicked")
+if st.button("pipe-maze"):
+    pipe_maze.main()
 if st.button("test2"):
     st.write("Test2 clicked")
 if st.button("test3"):
---
+++ pipe_maze.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Pipe Maze")
+    st.write("This is a placeholder for the pipe maze generator.")
+
+if __name__ == "__main__":
+    main()
