fd67cb6026b7bc68638e66ff7944014aa4752e22
sirmagnick <magnick@interia.pl>
2025-06-30 15:07:10 +0200

    Add Streamlit start screen with test buttons

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("activity books generator")
+
+if st.button("test1"):
+    st.write("Test1 clicked")
+if st.button("test2"):
+    st.write("Test2 clicked")
+if st.button("test3"):
+    st.write("Test3 clicked")
+if st.button("test4"):
+    st.write("Test4 clicked")
+
